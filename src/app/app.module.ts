dc031b4eee53bf1c43f8da9a675c549a0fb14a6d
Dmalonzo <malonzo.derek@gmail.com>
2020-03-26 23:30:58 -0700

    Basic Layout and routing addeed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SimpleComponent } from './simple/simple.component';
+import { AdvancedComponent } from './advanced/advanced.component';
+
+
+const appRoutes: Routes =[
+
+  {path: 'simple', component: SimpleComponent},
+  {path: 'advanced', component: AdvancedComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SimpleComponent,
+    AdvancedComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
